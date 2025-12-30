88be6863b6df0145c2bd765ae05952dd432fbb98
farjadA <111354332+farjadA@users.noreply.github.com>
2025-12-31 02:43:04 +0500

    Initial Commit

---
+++ stocksScreener/stocksScreenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  stocksScreenerApp.swift
+//  stocksScreener
+//
+//  Created by Farjad Ahmed on 31.12.25.
+//
+
+import SwiftUI
+
+@main
+struct stocksScreenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
